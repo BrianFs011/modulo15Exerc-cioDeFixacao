e5d9837f14e51a39975b7f9fa2fc2dc91b0bd020
Brian Silva <brianfsilva011@gmail.com>
2021-01-10 17:19:53 -0400

    Final

---
+++ src/model/application/Program.java
@@ -0,0 +1,44 @@
+package model.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[]args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double initBalance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withDrawLim = sc.nextDouble();
+			Account acc = new Account(number, holder, initBalance, withDrawLim);
+			
+			System.out.println();
+			
+			System.out.print("Enter amount for withdraw: ");
+			double withDraw = sc.nextDouble();
+			acc.withDraw(withDraw);
+			System.out.println(acc);
+		}
+		catch(DomainException e){
+			System.out.println(e.getMessage());
+		}
+		catch(RuntimeException e) {
+			System.out.println("unexpected error");
+		}
+		sc.close();
+	}
+}
